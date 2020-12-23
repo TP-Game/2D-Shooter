c44401c205a81e81eb5eb13d68a88677575e5f5c
Thanapon Ponpadung <ponpadun@sheridancollege.ca>
2020-12-23 10:42:11 +0700

    Add all axis player movement

--- 2D-Shooter/Assets/Script/Player.cs
+++ 2D-Shooter/Assets/Script/Player.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float playerSpeed;
+
+    private Rigidbody2D rigidBody;
+    private Vector2 moveVelocity;
+
+    private void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * playerSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + moveVelocity * Time.fixedDeltaTime);
     }
 }
